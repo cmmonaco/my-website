bdd16d61cd143611b2b28b2f8611ad09583b105c
cmmonaco <chrismonaco6@gmail.com>
2022-06-14 05:34:46 -0400

    additional files

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Entryway Display Case",
+        "excerpt":"This display case, located in Drew Charter School’s, Elementary Academy, was built as a modern replacement to an old, ugly platflorm. The goal was to provide a display area for the derby car while keeping with the industrial aesthetic in the rest of the entryway. The case is contructed of...","categories": [],
+        "tags": [],
+        "url": "/portfolio/display_case-2019/",
+        "teaser": "/assets/project-assets/entry_case-2019/entry_case_tease.jpg"
+      },{
+        "title": "Full Spectrum Cart",
+        "excerpt":"At Drew Charter School, we encouraged all teachers to utilize the digital fabrication tools in our makerspace. To make some tools more accessible, we tried to make them mobile and so laser cutter carts were born. This cart is specifically designed to house a Full Spectrum laser cutter along with...","categories": [],
+        "tags": [],
+        "url": "/portfolio/full_spectrum_cart-2017/",
+        "teaser": "/assets/project-assets/full_spectrum_cart-2017/fs_cart_tease.jpg"
+      },{
+        "title": "Glowforge Laser Cutter Cart",
+        "excerpt":"At Drew Charter School, we encouraged all teachers to utilize the digital fabrication tools in our makerspace. To make some tools more accessible, we tried to make them mobile and so laser cutter carts were born. This cart is specifically designed to house a Glowforge laser cutter along with room...","categories": [],
+        "tags": [],
+        "url": "/portfolio/glowforge_cart_2019/",
+        "teaser": "/assets/project-assets/glowforge_cart-2019/glowforge_cart_tease.jpg"
+      },{
+        "title": "Magnetic Separator Rack",
+        "excerpt":"Magnetic tube racks from commercial lab suppliers can costs hundreds of dollars. But if you have a 3D printer you can make your very own for a fraction of that price! These racks are designed to support two sides of six 1.5-2.0 mL microcentrifuge tubes. They use long, rectangular magnets...","categories": [],
+        "tags": [],
+        "url": "/portfolio/mag_rack_2021/",
+        "teaser": "/assets/project-assets/mag_rack-2021/mag_rack_tease.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/12/07/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "My Test Post",
+        "excerpt":"This is a blog Post  Hi this a test post. I’m desperately trying to get things working :(   ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/12/08/My-Test-Post.html",
+        "teaser": null
+      }]
